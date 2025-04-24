b5c512981d223b79a0aa9d582b86270e94bfd70e
Dr-MAhmed <dr.muhammadahmed05@gmail.com>
2025-04-24 21:51:15 +0500

    background  image add page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      Mochiii sybasdasdasd
-    </div>
+    <>
+      <div className='w-full h-full'>
+        <h1 className='text-3xl text-[#5C3810] font-bold text-center mt-20'>Welcome to the Page</h1>
+      </div>
+    </>
   )
 }
 
