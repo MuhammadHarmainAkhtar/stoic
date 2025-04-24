fd54ea3284bdda587e18f2f6317ccfac749914de
Dr-MAhmed <dr.muhammadahmed05@gmail.com>
2025-04-25 00:14:37 +0500

    bruney Bold Version added

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,6 +5,7 @@ const page = () => {
     <>
       <div className='w-full h-full'>
         <h1 className='text-4xl text-[#5C3810] font-bold font-[bruneyFont] text-center mt-20'>Welcome to the Page</h1>
+        <h2 className='text-4xl text-[#5C3810] font-bold font-[bruneyBold] text-center mt-20'>Welcome to the Bruney</h2>
       </div>
     </>
   )
