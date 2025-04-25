916c266cca032d39e8107137d610b70d24b5c6a4
muhammadharmainakhtar <mrharmain9@gmail.com>
2025-04-25 15:01:38 +0500

    navbar and hero

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,21 +1,12 @@
 import React from "react";
-import VintageButtons from "./components/vintage-button";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 const page = () => {
   return (
     <>
-      <main className="w-full h-full">
-        <h1
-          style={{ fontFamily: "Germania" }}
-          className="text-8xl text-[#5C3810] text-center mt-20"
-        >
-         welcome to the page
-        </h1>
-        <div className="mt-20 flex flex-col gap-5">
-          <VintageButtons className="font-bold text-3xl" name="get started" />
-          <VintageButtons className="font-bold text-3xl" name="learn more" />
-        </div>
-      </main>
+      <Navbar />
+      <Hero />
     </>
   );
 };
