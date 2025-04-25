7a815b8f60913970e4965f99759116d7c7a8f491
muhammadharmainakhtar <mrharmain9@gmail.com>
2025-04-25 13:02:52 +0500

    Successfully added vintage-button.tsx all thanks to Ahmed

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import VintageButtons from './components/vintage-button'
 
 const page = () => {
   return (
     <>
-      <div className='w-full h-full'>
-        <h1 className='text-4xl text-[#5C3810] font-bold font-[bruneyFont] text-center mt-20'>Welcome to the Page</h1>
-        <h2 className='text-4xl text-[#5C3810] font-bold font-[bruneyBold] text-center mt-20'>Welcome to the Bruney</h2>
-      </div>
+      <main className='w-full h-full'>
+        <h1 style={{ fontFamily: 'MorrisRoman' }} className='text-8xl text-[#5C3810] font-bold font-[bruneyFont] text-center mt-20'>Welcome to the Page</h1>
+        <h2 className=' text-8xl text-[#5C3810] font-bold text-center mt-20'>Welcome to the Bruney</h2>
+      <VintageButtons className='mt-10 font-MorrisRoman' name="GET STARTED"/>
+      <VintageButtons className='mt-10' name="LEARN MORE"/>
+      </main>
     </>
   )
 }
