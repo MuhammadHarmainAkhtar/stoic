d3321bd0380321d2429706f4298fee867bacd265
Dr-MAhmed <dr.muhammadahmed05@gmail.com>
2025-04-26 22:31:28 +0500

    background img added for smaller screens

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from "react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
@@ -6,8 +7,20 @@ import Image from "next/image";
 const Page = () => {
   return (
     <main className="min-h-screen w-full relative">
-      {/* Background Image */}
-      <div className="fixed inset-0 w-full h-full z-0">
+      {/* Background Image for small screens */}
+      <div className="absolute inset-0 w-full h-full z-0 block md:hidden">
+        <Image
+          src="/parchedbackground.jpg"
+          alt="Stoic background mobile"
+          fill
+          priority
+          className="object-cover"
+          quality={100}
+        />
+      </div>
+
+      {/* Background Image for medium screens and up */}
+      <div className="absolute inset-0 w-full h-full z-0 hidden md:block">
         <Image
           src="/stoicbackground.png"
           alt="Stoic background"
