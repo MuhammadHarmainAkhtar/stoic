a3c1cc8e8016abb89eac31243c7e72ea42cdafc9
muhammadharmainakhtar <mrharmain9@gmail.com>
2025-05-06 18:03:08 +0500

    starting working on Tribe

--- apps/web/app/settings/page.tsx
+++ apps/web/app/settings/page.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>settings</div>
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-5xl font-bold text-center font-[bruneyfont]">COMING SOON...</h1>
+    </div>
   )
 }
 
